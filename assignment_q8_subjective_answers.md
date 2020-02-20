f64f90ef8c3ea528ecef42708776ce52239b8053
rkreddy99 <rkreddy99@users.noreply.github.com>
2020-02-20 18:25:44 +0530

    done

--- assignment_q8_subjective_answers.md
+++ assignment_q8_subjective_answers.md
@@ -1,8 +1,12 @@
 # ES654-2020 Assignment 3
 
-*Your Name* - *Your Roll Number*
+*Rama Krishna* - *17110094*
 
 ------
 
-> Write the answers for the subjective questions here
+- Theoretical Time Complexity of normal equation is given by, `O(ND²) + O(D³)`
+- Theoretical Time complexity of gradient descent is given by `O(tND), t ∈ Ζ⁺`
 
+Here is the attached plot of comparision of time complexities.
+
+![time](Images\Q8time.png)
--- q8_compare_time.py
+++ q8_compare_time.py
@@ -10,7 +10,9 @@ np.random.seed(42)
 N = [1000*i for i in range(1,5)]
 P = 500
 grad_time = []
+theor_grad = []
 norm_time = []
+theor_norm = []
 for i in range(len(N)):
   X = pd.DataFrame(np.random.randn(N[i], P))
   y = pd.Series(np.random.randn(N[i]))
@@ -19,21 +21,28 @@ for i in range(len(N)):
   LR.fit_vectorised(X.copy(),y.copy(),n_iter = 100,batch_size=N[i])
   LR.predict(X.copy())
   b = time.time()
+  t = (N[i]*(P+1)**2 + (P+1)**3)*10**-9
+  theor_grad.append(t)
   LR1 = copy.deepcopy(LinearRegression(fit_intercept=True))
   c = time.time()
   LR1.fit_normal(X.copy(),y.copy())
   LR1.predict(X.copy())
   d = time.time()
+  r = 100*N[i]*P*10**-9
+  theor_norm.append(r)
   grad_time.append(b-a)
   norm_time.append(d-c)
 print(grad_time,'\n')
 print(norm_time, '\n')
 grad_time, norm_time = np.array(grad_time), np.array(norm_time)
+theor_grad, theor_norm = np.array(theor_grad), np.array(theor_norm)
 N = np.array(N)
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(N,grad_time, label = 'gradient descent')
-ax.plot(N, norm_time, label = 'normal')
+ax.plot(N,grad_time, label = 'obs gradient descent')
+ax.plot(N, norm_time, label = 'obs normal')
+ax.plot(N,theor_grad, label='theor gradient descent')
+ax.plot(N,theor_norm, label = 'theor normal')
 # ax.set_yscale('log')
 ax.set_ylabel('time(s)')
 ax.set_xlabel('No. of samples')
